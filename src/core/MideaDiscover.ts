6a9ad3c6ff3af9e67bac837347058e37f4614e93
kovapatrik <kovapatrik@gmail.com>
2023-10-31 16:59:06 +0100

    fix: prettier ignore

--- src/core/MideaDiscover.ts
+++ src/core/MideaDiscover.ts
@@ -193,7 +193,8 @@ export default class Discover extends EventEmitter {
         throw new Error(`Error while decrypting data: ${err}`);
       }
 
-      // eslint-disable-next-line max-len, prettier/prettier
+      // prettier-ignore
+      // eslint-disable-next-line max-len
       const ip_address = `${decrypted_buffer.readUint8(3)}.${decrypted_buffer.readUint8(2)}.${decrypted_buffer.readUint8(1)}.${decrypted_buffer.readUint8(0)}`;
       const port = decrypted_buffer.readUIntLE(4, 2);
 
